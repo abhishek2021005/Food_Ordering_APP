a368df6099d691f1b2f18b87be6cea89a7f17e8a
Abhishek Kumar <abhishek2021005@gmail.com>
2024-06-03 00:35:10 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -10,8 +10,19 @@ export const RES_API_URL =
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
   );
 
+// export const RES_MENU_API =
+//   "https://corsproxy.io/?" +
+//   encodeURIComponent(
+//     "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001"
+//   );
+
+
+
+
+
 export const RES_MENU_API =
   "https://corsproxy.io/?" +
   encodeURIComponent(
-    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001"
+    "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.73390&lng=76.78890"
   );
+
